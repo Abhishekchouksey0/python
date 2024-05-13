4a7c7a9573bf833a5b9cf8ee4aed4f8057b89cb5
you example <you@example.com>
2024-05-13 13:24:32 +0530

    Enlarged screen size

--- New.py
+++ New.py
@@ -5,8 +5,8 @@ import random
 pygame.init()
 
 # set screen width and height
-screen_width = 800
-screen_height = 600
+screen_width = 1000
+screen_height = 800
 
 # create the screen
 screen = pygame.display.set_mode((screen_width, screen_height))
